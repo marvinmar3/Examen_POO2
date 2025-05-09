0bc1a9c0d5377cb227a5eca8607540089431781a
marvin <martinezmartinezmarvin@gmail.com>
2025-05-09 17:24:30 -0600

    segundo commit, se creo secureclient

---
+++ src/main/java/com/examenpoo/SecureClient.java
@@ -0,0 +1,30 @@
+package com.examenpoo;
+
+import javax.net.ssl.*;
+import java.io.*;
+
+public class SecureClient {
+
+    private static final String HOST = "localhost";
+    private static final int PORT = 5000;
+
+    public static void main(String[] args)  throws Exception {
+        System.setProperty("javax.net.ssl.trustStore", "server.keystore");
+        System.setProperty("javax.net.ssl.trustStorePassword", "contrasena123");
+
+        SSLSocketFactory socketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+
+        try (SSLSocket socket = (SSLSocket) socketFactory.createSocket(HOST, PORT);
+             ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+             ObjectInputStream in = new ObjectInputStream(socket.getInputStream())){
+
+            System.out.println("Conexion segura establecida con el servidor.");
+
+            out.writeObject("Hola desde el cliente🫡");
+            System.out.println("Objeto enviado.");
+
+            Object response = in.readObject();
+            System.out.println("Respuesta del servidor: "+response.toString());
+        }
+    }
+}
